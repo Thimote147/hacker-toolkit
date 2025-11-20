462af3f0cf1597ac17a4e14c3f1f6b0a3af75c8c
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2025-11-21 00:18:26 +0100

    added delete.sh to restricted/scripts

---
+++ solutions/restricted/scripts/delete.sh
@@ -0,0 +1,7 @@
+    <!-- ❌ À SUPPRIMER -->
+    <!-- /usr/share/nginx/html/custom_50x.html -->
+
+   <!-- ❌ À SUPPRIMER -->
+   <script>
+     console.log('... developer ... : JHtGTEFHX0Z1enp5RE9TfQ==');
+   </script>
